fad977055f38b16e997f5569a65c2dbf42c1d951
red1bluelost <msweston12@cox.net>
2019-11-24 17:26:44 -0500

    Places the code in a properly labeled final code sumo 2 code.

--- Sumo_2_Code_Final/Sumo_2_Code_Final.ino
+++ Sumo_2_Code_Final/Sumo_2_Code_Final.ino
@@ -39,6 +39,21 @@ bool dontRepeat = false;  //for edge sensing causing a breaking in code
 //bool to be used to only run the starting strategy on the first loop
 bool startStrat;   //runs the starting strategy
 
+//class to create objects that allow checking if a certain amount of time passed
+class TimeTest {
+  private:
+  long int preTime;
+  long int interval;
+  public:
+  TimeTest();
+  void SetIntvl(long int);
+  bool TimePassTest();
+  void ResetTime();
+};
+
+//create instances of TimeTest for testing if time has pas
+TimeTest TTEvadeSide;
+
 void setup() {
   Serial.begin (9600);        //set up a serial connection with the computer
 
@@ -65,39 +80,19 @@ void setup() {
 
   //do starting strategy only after set up
   startStrat = true;
+
+  //set TimeTest intervals
+  TTEvadeSide.SetIntvl(10000);
 }
 
 void loop() {
-  //Strategy plays out only on the first run
-  if(startStrat){
-
-    //0.1 Turn left
-    turnHold('l',true,55);
-
-    //0.2 move to edge of the ring
-    straightMove(16,90,'f');
-  
-    //0.3 Turn right and start scanning
-    turnHold('r',false,0);
-    delay(750); //don't scan off map on first run
-    startStrat = false; //never repeat starting strategy
-  }
-  //Starting point for all loops after first
-
-  //1.0 spin and scan for another robot
-  spinAndScan(20);  //20 inch range detection
-  
-  //2.0 If something is seen within 24 inches, charge
-  charge(90,'f');
-
-  //3.0 loop so the charge does not stop
-  do{
-    dontRepeat = edgeSense(); //make sure he does not run off the map
-  }while(!dontRepeat);  //keep charging if edgeSense doesn't see the edge
-  //go back to the beginning of the loop if the edge was hit while charging at a robot
+  startStrategy(startStrat);
+  spinAndScan(24);
 }
 
 //------------------FUNCTIONS-------------------------------
+
+//----Functions to be used in other functions---------------
 
 //RETURNS THE DISTANCE MEASURED BY THE HC-SR04 DISTANCE SENSOR
 void getDistance(DistanceSensor& sensor)
@@ -119,8 +114,8 @@ void getDistance(DistanceSensor& sensor)
   
   sensor.distance = calculatedDistance;              //send back the distance that was calculated
 
-      Serial.print(sensor.distance);     //print the distance that was measured
-    Serial.println(" in");      //print units after the distance
+  Serial.print(sensor.distance);     //print the distance that was measured
+  Serial.println(" in");      //print units after the distance
 }
 
 //function to use when writing to the motors to move at the same speed
@@ -133,10 +128,12 @@ void congruentMove(int moveSpeed, bool forward) {
     servo_R.write(90+moveSpeed);
     servo_L.write(90-moveSpeed);
   }
-} //end motorMove()
+} //end congruentMove()
+
+//----Functions to be used for guiding robot actions----------------------------------
 
 //Input desired distance (in) and speed 1-90 and direction
-void straightMove(float range, float moveSpeed, char goOrBack){ //input range (inch), speed (1-90), and f or b for direction
+void straightMove(float range, float moveSpeed, bool forward){ //input range (inch), speed (1-90), and f or b for direction
 
   //calculate time needed for a chosen distance at a chosen speed
   float distanceFromRange;
@@ -146,29 +143,10 @@ void straightMove(float range, float moveSpeed, char goOrBack){ //input range (i
     distanceFromRange = (range * (5000/32.5));
 
   //set motors to move at the speed
-  switch(goOrBack){
-    case 'f': //move forward
-    congruentMove(moveSpeed,true);
-    break;
-    case 'b': //move back
-    congruentMove(moveSpeed,false);
-    break;
-  } //end switch to move forward or back at a certain speed
+  congruentMove(moveSpeed, forward);
 
   //move for certain time
-  delay(distanceFromRange);
-}
-
-void charge(float moveSpeed, char goOrBack){ //set to move without any delays
-  //set motors to move at the speed
-  switch(goOrBack){
-    case 'f': //move forward
-    congruentMove(moveSpeed,true);
-    break;
-    case 'b': //move back
-    congruentMove(moveSpeed,false);
-    break;
-  } //end switch to move forward or back at a certain speed
+  safeDelay(distanceFromRange);
 }
 
 //a function to turn the robot and with the option of rotating for a specific angle of rotation
@@ -184,7 +162,7 @@ void turnHold(char lORr, bool doAngle, float angle) { //turn left or right, if d
       break;
   }
   if(doAngle){  //turn for certain amount of degrees
-    delay((angle/360)*3350);
+    safeDelay((angle/360)*3350);
     congruentMove(0,true); //stop after rotation finishes
   }
 }
@@ -203,8 +181,57 @@ void moveAndTurn(char lORr, float turnRate){ //curved movement, turnRate 1-180 f
   }
 }
 
+void spinAndScan(int range){
+    do{     //spin and scan loop
+    getDistance(FrontDist);  //check for robot in front sensor
+    if (FrontDist.distance <= range){
+      break;
+    }
+    getDistance(LeftDist);
+    getDistance(RightDist);
+    if (LeftDist.distance < range) {
+      turnHold('l',false,0);
+    } else if (RightDist.distance < range) {
+      turnHold('r',false,0);
+    }
+    
+    if (matSense() || evadeSide()){  //make sure not to run off the edge of the mat
+      return;
+    }
+  }while(FrontDist.distance > range);   //keep scanning until something is seen within 24 inches
+  congruentMove(90,true);
+  holdCommand();
+}
+
+//Strategy plays out only on the first run
+void startStrategy(bool &firstTime){
+  if (!firstTime) {  //check if it is first time to break or not
+    return;
+  }
+  turnHold('l',true, 55); //turn left
+  straightMove(16,90,'f');  //move to edge of the ring
+  turnHold('r',false,0);  //turn right to start scanning
+  firstTime = false;  //never repeat starting stragtegy
+}
+
+//function to keep the most recent command running unless an evasive measure is necessary
+void holdCommand() {
+  for(;;){
+    if (matSense() || evadeSide()){  //make sure not to run off the edge of the mat
+      break;
+    }
+  }
+}
+
+//----Safety functions to be used to perform evasive measures----------------------------
+
 //function to check edge sensors and perform evasion movement and tells code if it was sensed
 bool edgeSense(){
+  getDistance(FrontDist);
+  if (FrontDist.distance < 4 || FrontDist.distance > 900) {
+    return false;
+  }
+  
   //read sensors
   fRightSensor.sensorState = digitalRead(fRightSensor.Sensor);  //read right sensor
   fLeftSensor.sensorState = digitalRead(fLeftSensor.Sensor);   //read left sensor
@@ -258,8 +285,6 @@ bool backSense(){
     //rotate away from the edge
     servo_R.write(180);    // spin right motor backwards
     servo_L.write(180);    // spin Left motor forwards
-    servo_L.write(0);  //left motor Clockwise for reverse
-    servo_R.write(0);   //right motor clockwise forward
     //delay(700);  //  .25 second delay
     return true;  //return that the edge was hit
   }
@@ -267,23 +292,61 @@ bool backSense(){
     return false; //return that the edge was not hit
 }
 
-void spinAndScan(int range){
-    do{     //spin and scan loop
-    getDistance(FrontDist);  //check for robot in front sensor
-    edgeSense();  //make sure not to run off the edge of the mat
-  }while(FrontDist.distance > range);   //keep scanning until something is seen within 24 inches
+bool matSense() {
+  return edgeSense() || backSense();
 }
 
-void evadeSide() {
-  int near = 3;   //distance of what will be considered near
+bool evadeSide() {
   getDistance(LeftDist);
   getDistance(RightDist);
-  if (LeftDist.distance < 3  && LeftDist.distance > 0.2){ //if something near on left do evasive manuver
-    servo_R.write(180);
-    servo_L.write(90);
+  if (TTEvadeSide.TimePassTest()){
+    if (LeftDist.distance < 3  && LeftDist.distance > 0.2){ //if something near (< 3 inch) on left do evasive manuver
+      servo_R.write(180);
+      servo_L.write(90);
+      delay(1600);
+      straightMove(6, 90, false);
+      turnHold('l',false,0);
+      TTEvadeSide.ResetTime();
+      return true;
+    }
+    if (RightDist.distance < 3 && RightDist.distance > 0.2){ //if something near (< 3 inch) on right do evasive manuver
+      servo_R.write(90);
+      servo_L.write(0);
+      delay(1600);
+      straightMove(6, 90, false);
+      TTEvadeSide.ResetTime();
+      turnHold('r',false,0);
+      return true;
+    }
   }
-  if (RightDist.distance < 3 && RightDist.distance > 0.2){ //if something near on right do evasive manuver
-    servo_R.write(90);
-    servo_L.write(0);
+  return false;
+}
+
+void safeDelay(long int intvl) {
+  long int timeTest, currentTime;
+  timeTest = intvl + millis();
+  do{
+    currentTime = millis();
+    if (matSense() || evadeSide()){
+      break;
+    }
+  }while(currentTime < timeTest);
+}
+
+//----Time Test class methods---------------------------------
+
+TimeTest::TimeTest() {
+  preTime = 0;
+}
+void TimeTest::SetIntvl(long int i) {
+  interval = i;
+}
+bool TimeTest::TimePassTest(){
+  if (millis() - interval < preTime) {
+    return false;
   }
+  return true;
+}
+void TimeTest::ResetTime() {
+  preTime = millis();
 }
